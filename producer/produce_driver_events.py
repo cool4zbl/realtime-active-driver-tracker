9ff229f0df89d1440a706154d935d53a52f6938e
Binliu Zhang <binliu.zhang@gmail.com>
2025-04-03 23:37:54 +0200

    setup kafka producer; close #2

--- producer/produce_driver_events.py
+++ producer/produce_driver_events.py
@@ -2,14 +2,17 @@ from kafka import KafkaProducer
 import json
 import time
 import random
+import os  # new import
 
 producer = KafkaProducer(
     bootstrap_servers='localhost:9092',
     value_serializer=lambda v: json.dumps(v).encode('utf-8')
 )
 
-cities = ['Amsterdam', 'Rotterdam', 'Utrecht', 'Eindhoven']
+cities = ['Amsterdam', 'Rotterdam', 'Utrecht', 'Eindhoven', 'Enschede']
 drivers = [f'driver_{i}' for i in range(1, 101)]
+
+topic = os.getenv('KAFKA_TOPIC', 'driver-events')  # new topic variable
 
 while True:
     event = {
@@ -17,6 +20,6 @@ while True:
         "city": random.choice(cities),
         "timestamp": int(time.time())
     }
-    producer.send('driver-events', value=event)
+    producer.send(topic, value=event)
     print(f"Produced: {event}")
     time.sleep(1)
