46d9314110dacd177b3ced5ff481aaf71c028b2e
Binliu Zhang <binliu.zhang@gmail.com>
2025-06-03 21:56:55 +0200

    update flink job, rm unused files

--- backend-java/flink-demo/entry.sh
+++
@@ -1,9 +0,0 @@
-export FLINK_VERSION=1.20.1
-export MAIN_CLASS=org.example.WindowWordCount     # 你的入口类
-export JAR_PATH=./target/flink-demo-1.0-SNAPSHOT.jar  # JAR包路径
-
-docker run --rm \
-  -p 8081:8081 \
-  -v ${JAR_PATH}:/opt/flink/usrlib/flink-demo-1.0-SNAPSHOT.jar \
-  flink:${FLINK_VERSION} \
-  standalone-job
--- backend-java/flink-demo/src/main/java/org/example/task/HeatmapJob.java
+++ backend-java/flink-demo/src/main/java/org/example/task/HeatmapJob.java
@@ -44,7 +44,7 @@ public class HeatmapJob {
                 // 1. 实时作业：latest()，部署快，不拖积压
                 // 2. 离线回填作业：单独启动一个 job，earliest() → 处理完写 HDFS／S3，然后关掉。
                 // 首次上线且必须补历史 → 可以先用 earliest() 全量跑一次，再把消费者 group 重置到最新 offset，再切到 latest()
-                .setStartingOffsets(OffsetsInitializer.earliest()) // Use latest for real-time processing, not to process old data
+                .setStartingOffsets(OffsetsInitializer.latest()) // Use latest for real-time processing, not to process old data
                 .setValueOnlyDeserializer(new SimpleStringSchema())
                 .build();
 
